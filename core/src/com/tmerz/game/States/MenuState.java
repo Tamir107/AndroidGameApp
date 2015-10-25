6889249675934fbc0fc25b0b07895dec1f409708
Tamir Razon <tamir.razon@gmail.com>
2015-10-26 00:06:07 +0200

    Added Textures

--- core/src/com/tmerz/game/States/MenuState.java
+++ core/src/com/tmerz/game/States/MenuState.java
@@ -1,13 +1,17 @@
 package com.tmerz.game.States;
 
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.tmerz.game.FlappyBird;
 
-/**
- * Created by תמיר on 25/10/2015.
- */
+
 public class MenuState extends State {
+    private Texture background;
+    private Texture playBtn;
     public MenuState(GameStateManager gsm) {
         super(gsm);
+        background = new Texture("bg.png");
+        playBtn = new Texture("playBtn");
     }
 
     @Override
@@ -22,6 +26,9 @@ public class MenuState extends State {
 
     @Override
     public void render(SpriteBatch sb) {
-
+        sb.begin();
+        sb.draw(background, 0, 0, FlappyBird.WIDTH, FlappyBird.HEIGHT);
+        sb.draw(playBtn, (FlappyBird.WIDTH / 2) - (playBtn.getWidth() / 2), FlappyBird.HEIGHT / 2);
+        sb.end();
     }
 }
