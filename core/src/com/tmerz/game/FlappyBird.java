e22c1909552c62146336e575f540a10d9db067b0
Tamir Razon <tamir.razon@gmail.com>
2015-10-25 22:49:31 +0200

    Initial commit

---
+++ core/src/com/tmerz/game/FlappyBird.java
@@ -0,0 +1,34 @@
+package com.tmerz.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.tmerz.game.States.GameStateManager;
+import com.tmerz.game.States.MenuState;
+
+public class FlappyBird extends ApplicationAdapter {
+	public static final int WIDTH = 480;
+	public static final int HEIGHT = 800;
+	public static final String TITLE = "Flappy Bird";
+	private GameStateManager gsm;
+	private SpriteBatch batch;
+	Texture img;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		img = new Texture("badlogic.jpg");
+		gsm = new GameStateManager();
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+		gsm.push(new MenuState(gsm));
+	}
+
+	@Override
+	public void render() {
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		gsm.update(Gdx.graphics.getDeltaTime());
+		gsm.render(batch);
+	}
+}
---
+++ core/src/com/tmerz/game/States/MenuState.java
@@ -0,0 +1,27 @@
+package com.tmerz.game.States;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+/**
+ * Created by תמיר on 25/10/2015.
+ */
+public class MenuState extends State {
+    public MenuState(GameStateManager gsm) {
+        super(gsm);
+    }
+
+    @Override
+    public void handleInput() {
+
+    }
+
+    @Override
+    public void update(float dt) {
+
+    }
+
+    @Override
+    public void render(SpriteBatch sb) {
+
+    }
+}
